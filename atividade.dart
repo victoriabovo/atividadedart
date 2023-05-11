f642990eb2afc4284e5d3bbb498be04af0e9e99c
VICTORIA REGINA BOVO BATISTON <victoria.9946@aluno.pr.senac.br>
2023-05-11 15:23:05 -0300

    mapa

--- atividade.dart
+++ atividade.dart
@@ -2,9 +2,9 @@ void main() {
   List<String> meusLivros = ['Talvez um dia', 'Assim que acaba', 'Uma segunda chance'];
 
   meusLivros.add('Todas as suas Imperfeições'); //adicionar
-
+print(meusLivros);
   meusLivros.remove('Talvez um dia'); //remover
-
+print(meusLivros);
   
   if (meusLivros.contains('Todas as suas Imperfeições')) { //pesquisar 
     print('Contém esse livro na lista');
---
+++ mapa.dart
@@ -0,0 +1,9 @@
+void main() {
+  Map<int, String> codigo = {
+    11: "São Paulo",
+    19: "Campinas",
+    41: "Curitiba",
+    43: "Ivaiporã",
+  };
+  print(codigo[11]);
+}
